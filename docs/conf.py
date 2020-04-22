e440358c926a213d9332c844e8ac5277483820e5
Rob Maccallum <robmaccallum@gmail.com>
2020-04-22 21:26:07 +0200

    updates rtd config

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,8 @@ master_doc = 'index'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.autosectionlabel']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.autosectionlabel',
+              'readthedocs_ext.readthedocs']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
