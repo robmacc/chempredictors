442453a26b7958ee244b6c5a7868d8ea52528927
Rob Maccallum <robmaccallum@gmail.com>
2020-04-22 14:12:50 +0200

    fixes imports

--- src/dataset.py
+++ src/dataset.py
@@ -1,11 +1,11 @@
 import rdkit.Chem
-import src.molecular
+from . import molecular
 import torch_geometric
 
 
 class Dataset(torch_geometric.data.Dataset):
     def __init__(self, file, labels, mol_property):
-        self.data = [src.molecular.molToGraph(m, mol_property, labels) for m in
+        self.data = [molecular.molToGraph(m, mol_property, labels) for m in
                      rdkit.Chem.SDMolSupplier(file)]
 
     def __getitem__(self, index):
